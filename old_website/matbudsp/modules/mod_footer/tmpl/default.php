3b72bab16a42bd7cb8528d9de3bdecbc95fd9855
neloduka-sobe <nelodukasobe@gmail.com>
2025-09-01 15:48:31 +0200

    Fix

--- old_website/matbudsp/modules/mod_footer/tmpl/default.php
+++
@@ -1,4 +0,0 @@
-<?php // no direct access
-defined('_JEXEC') or die('Restricted access'); ?>
-<div><?php echo $lineone; ?></div>
-<div><?php echo JText::_( '' ); ?></div>
--- old_website/matbudsp/templates/business-portal/html/mod_syndicate/default.php
+++
@@ -1,4 +0,0 @@
-<?php // no direct access
-defined('_JEXEC') or die('Restricted access'); ?>
-<a href="<?php echo $link ?>"><?php echo $img ?></a>
-<a href="<?php echo $link ?>"><?php echo $params->get('text') ?></a>
